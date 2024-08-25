412751a9f6eaff387169ddb28cbd2de0629afb89
Renan de Souza Rodrigues <renanrodrigues7110@gmail.com>
2024-08-24 21:00:46 -0300

    adicionando a API fast e request

--- src/FastAPI_python/main.py
+++ src/FastAPI_python/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 """uvicorn main:app --reload -> para gerar link da api"""
 """uvicorn send_api:app --reload --host 0.0.0.0 --port 8000"""
@@ -27,7 +28,11 @@ def pegar_item(id: int):
     return conteudo_api[id]
 
 
-@app.post("/itens/{id}/{prod}/{acrecentado}")
+@app.post("/itens/{id}/{prod}")
 def adicionar_item(id: int, prod: str, acrecentado: str):
     conteudo_api[id][prod].append(acrecentado)
     return conteudo_api[id][prod]
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, host="127.0.0.1", port=8080)
