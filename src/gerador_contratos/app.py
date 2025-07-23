d16dbc9c69ecc7854d97c2c101a7704d67d9d51a
Renan-RodriguesDEV <renanrodrigues7110@gmail.com>
2025-07-22 21:32:20 -0300

    feat: habilitar CORS no aplicativo Flask e ajustar configuração de execução

--- src/gerador_contratos/app.py
+++ src/gerador_contratos/app.py
@@ -1,10 +1,12 @@
 import os
 
 from flask import Flask, jsonify, render_template, request, send_file
+from flask_cors import CORS
 
 from views.handler_document import create_document
 
 app = Flask(__name__, template_folder="templates")
+CORS(app)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -29,4 +31,4 @@ def download(file):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
